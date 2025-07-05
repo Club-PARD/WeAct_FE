08f253e5d2cafc71457857b93cabd643be6a5a77
현승훈 <tmdgns4970@handong.ac.kr>
2025-07-05 15:14:33 +0900

    [Feat: Final page 수정중]

---
+++ WeAct/WeAct-Watch Watch App/WeAct_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeAct_WatchApp.swift
+//  WeAct-Watch Watch App
+//
+//  Created by 현승훈 on 7/4/25.
+//
+
+import SwiftUI
+
+@main
+struct WeAct_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
